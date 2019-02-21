738eb08765377beabc4f05ec68aeadf66e31a02b
Duarte <duartetoxic@hotmail.com>
2019-02-21 18:44:08 +0000

    Yarnfile contents represented by YarnfileNodeScript file; Convertion method added in YarnfileManager file

---
+++ Assets/Storytelling/Scripts/YarnRelated/StorytellingYarnfileManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReGoap.Core;
+using System.IO;
+
+public class StorytellingYarnfileManager : MonoBehaviour
+{
+    string Path = "Yarnfiles/preview.yarn"; // TO DO
+    TextAsset YarnfileAsset;
+    Dictionary<string, StorytellingYarnfileNodeScript> Nodes;
+
+    void Start()
+    {
+        Nodes = new Dictionary<string, StorytellingYarnfileNodeScript>();
+
+        ReadYarnfile();
+        List<string> lines = new List<string>(YarnfileAsset.text.Split('\n'));
+        CreateNodesFromYarnfile(lines);
+    }
+
+    void ReadYarnfile()
+    {
+        print(Path);
+        YarnfileAsset = (TextAsset) Resources.Load(Path);
+    }
+
+    void CreateNodesFromYarnfile(List<string> lines)
+    {
+        string nodeNameTemp = "";
+        string nodeTemp = "";
+        
+        foreach(string line in lines)
+        {
+            if (line.Contains("title: "))
+            {
+                nodeNameTemp = GetNameForNode(line);
+            }
+            nodeTemp += line + "\n"; // stack node
+            if (line == "===\r") // flush node
+            {
+                StorytellingYarnfileNodeScript yarnNode = new StorytellingYarnfileNodeScript(nodeNameTemp, nodeTemp);
+                AddNode(yarnNode);
+
+                nodeTemp = "";
+            }
+        }
+
+        print(Nodes["Start.Start"].GetBody());
+        print(Nodes["Grades.good"].GetBody());
+    }
+
+    string GetNameForNode(string firstLine)
+    {
+        string name = firstLine.Replace("title: ", string.Empty);
+        name = name.Replace("\r", string.Empty);
+        return name;
+    }
+
+    void AddNode(StorytellingYarnfileNodeScript node)
+    {
+        Nodes.Add(node.GetName(), node);
+    }
+}
---
+++ Assets/Storytelling/Scripts/YarnRelated/StorytellingYarnfileNodeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorytellingYarnfileNodeScript : MonoBehaviour
+{
+    string Name;
+    string Body;
+
+
+
+    public StorytellingYarnfileNodeScript(string name, string body)
+    {
+        Name = name;
+        Body = body;
+    }
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public string GetBody()
+    {
+        return Body;
+    }
+}
